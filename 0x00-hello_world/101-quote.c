2093b1b465b88ef1087e6d4dc771bd4b093406de
opaque-maniac <mbithimark8@gmail.com>
2023-03-16 15:30:08 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 1 when unsuccessfull
+ */
+
+int main(void)
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	printf("%s\n", msg);
+	return (1);
+}
