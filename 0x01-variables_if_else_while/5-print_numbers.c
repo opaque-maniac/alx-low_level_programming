b7dab8fab37cbe7dcaeca175e892d1ccddc3e3d9
opaque-maniac <mbithimark8@gmail.com>
2023-03-17 09:14:05 +0300

    Q5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	for (int i=0; i < 10; i++)
+	{
+		printf("%d",i);
+	}
+	printf('\n');
+	return (0);
+}
