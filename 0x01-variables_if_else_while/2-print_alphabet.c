a4b3c0dda6a238f00590143480239e436291a000
opaque-maniac <mbithimark8@gmail.com>
2023-03-17 08:20:41 +0300

    question two

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	char i = 'a';
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
