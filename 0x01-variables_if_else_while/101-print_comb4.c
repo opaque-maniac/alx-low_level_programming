c6d869564a0d14a1982192ddc32e0931ac3fbd9a
opaque-maniac <mbithimark8@gmail.com>
2023-03-17 11:49:24 +0300

    Question eleven

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	int digit1 = 0;
+
+	while (digit1 < 8)
+	{
+		int digit2 = digit1 + 1;
+		while (digit2 < 9)
+		{
+			int digit3 = digit2 + 1;
+			while (digit3 < 10)
+			{
+				putchar(48 + digit1);
+				putchar(48 + digit2);
+				putchar(48 + digit3);
+				if (digit1 < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				digit3++;
+			}
+			digit2++;
+		}
+		digit1++;
+	}
+	putchar('\n');
+	return (0);
+}
