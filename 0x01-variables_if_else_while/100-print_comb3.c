63f70e79149612f8f74b7f95cc64dfa6ac995e2c
opaque-maniac <mbithimark8@gmail.com>
2023-03-17 00:45:02 -0700

    Question ten

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i < 9)
+			{
+				putchar(48 + i);
+				putchar(48 + j);
+				if (i != 8 && j != 9)
+				{
+					putchar(' ');
+					putchar(',');
+				} else
+				{
+					break;
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
