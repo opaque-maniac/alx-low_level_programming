292616f6cd668dd8385ead0ce794d236c316848f
opaque-maniac <mbithimark8@gmail.com>
2023-03-17 01:10:17 -0700

    Question ten

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,19 +6,21 @@
  * Return: 0 when successfull
  */
 
-int main()
+int main(void)
 {
+	int i = 0;
+	int j = 0;
 	int counter = 0;
 
-	for (int i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (int j = 1; j < 10; j++)
+		for (j = 1; j < 10; j++)
 		{
-			putchar(i);
-			putchar(j);
+			putchar(48 + i);
+			putchar(48 + j);
 			putchar(' ');
 			counter++;
-			if (counter <= 89)
+			if (counter < 79)
 			{
 				putchar(',');
 			} else
