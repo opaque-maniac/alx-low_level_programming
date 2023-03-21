aeec008d15aa4f8af4cbc34aa2b11d97de44312b
opaque-maniac <mbithimark8@gmail.com>
2023-03-21 18:25:18 +0300

    Q7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function to test digits
+ * @c: This is the parameter to test
+ * Return: value of last digit
+ */
+
+int print_last_digit(int c)
+{
+	int last = c % 10;
+	
+	return (last);
+}
