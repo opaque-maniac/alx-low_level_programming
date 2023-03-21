bea37bb5bb36ce68ce7e4122d028a21758c3ad3c
opaque-maniac <mbithimark8@gmail.com>
2023-03-21 19:00:03 +0300

    Q9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - this is the function to print multiplication tables
+ * Return : void
+ */
+
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			_putchar((a * b) + '0');
+			if (b != 9)
+			{
+				_putchar(44);
+				_putchar(32)
+			} else
+			{
+				_putchar(36);
+			}
+		}
+		_putchar(10);
+	}
+}
