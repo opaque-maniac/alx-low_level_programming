555899b506a4da29b22ac3cd27d824f62e80fbd0
opaque-maniac <mbithimark8@gmail.com>
2023-03-22 09:42:27 +0300

    Question sixteen

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	int count = 0;
+	int i = 1;
+	int j = 2;
+	int temp;
+
+	while (count < 98)
+	{
+		printf("%d", a);
+		temp = a + b;
+		a = b;
+		b = temp;
+		count++;
+		if (count == 97)
+		{
+			printf("\n");
+		} else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
