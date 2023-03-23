cd735fb8cf34579a83439754fdadf72d953642a8
opaque-maniac <mbithimark8@gmail.com>
2023-03-23 11:55:42 +0300

    Question zero

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
