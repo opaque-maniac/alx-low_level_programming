7e0ffa42ab4496caefb95b32fc39b143319b907f
opaque-maniac <mbithimark8@gmail.com>
2023-03-24 02:39:58 +0300

    Q10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -24,9 +24,9 @@ void print_triangle(int size)
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size - 1; j++)
-				_putchar(' ');
-			for (k = 0; k <= i; k++)
 				_putchar('#');
+			for (k = 0; k <= i; k++)
+				_putchar(' ');
 			_putchar('\n');
 		}
 	}
