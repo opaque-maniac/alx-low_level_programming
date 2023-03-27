9a970393d5a5e29d31c02a7ea37b074c6e0bc6e5
opaque-maniac <mbithimark8@gmail.com>
2023-03-27 11:44:55 -0700

    Questio three

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - test function
+ * @str: string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
