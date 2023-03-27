ac7ad777082aafa30eed9b0f421b34186084798a
opaque-maniac <mbithimark8@gmail.com>
2023-03-28 01:16:32 +0300

    Question six

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,17 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
+	int j = 9;
 
-	while (str[i] != '\0')
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i++)
 	{
 		_putchar(str[i]);
-		i += 2;
 	}
 	_putchar('\n');
 }
