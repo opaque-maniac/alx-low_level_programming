eecbc60d3bb1207fcfdbdc600dc72d289398bee2
opaque-maniac <mbithimark8@gmail.com>
2023-03-27 12:19:00 -0700

    Question four

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - test function
+ * @s: string to reverse
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
