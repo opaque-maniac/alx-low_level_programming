21a51863d5dd7cfebf6d2da9bfc0311b07acb6ac
opaque-maniac <mbithimark8@gmail.com>
2023-03-28 01:29:16 +0300

    q6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,16 +12,9 @@ void puts2(char *str)
 	int i;
 	int len = strlen(str);
 
-	for (i = 0; i < len; i+= 2)
+	for (i = 0; i < len; i += 2)
 	{
-		if (str[i] == '\0')
-		{
-			break;
-		}
-		else
-		{
-			_putchar(str[i]);
-		}
-		_putchar('\n');
+		_putchar(str[i]);
 	}
+	_putchar('\n');
 }
