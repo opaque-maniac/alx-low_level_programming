213ea3334896e9e59cfcf6fc407cc2cb0dfb2df0
opaque-maniac <mbithimark8@gmail.com>
2023-04-14 15:30:29 -0700

    malloc is fun

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @j: memory to allocate
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int j)
+{
+	char *k;
+
+	k = malloc(j);
+	if (k == NULL)
+	{
+		exit(98);
+	}
+	return (k);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * function prototypes go down here
+ */
+
+int _putchar(char c);
+
+void *malloc_checked(unsigned int j);
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+int *array_range(int min, int max);
+
+void *_realloc(void *ptr, unsigned int size1, unsigned int size2);
+
+#endif
