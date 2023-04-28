99aa22bb86aa0c0fae49533760f9f704bde18ea4
opaque-maniac <mbithimark8@gmail.com>
2023-04-28 06:23:39 -0700

    First question

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: pointer to the start of the list.
+ *
+ * Return: the number of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+    size_t count = 0;
+
+    while (h != NULL)
+    {
+        printf("[%u] %s\n", h->len, h->str ? h->str : "(nil)");
+        count++;
+        h = h->next;
+    }
+
+    return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+This is a folder for low level programming data structures and algorithms
+Linked list to be in particular
