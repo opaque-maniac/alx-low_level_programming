53375a83e8d0888aeeb8cf07bd9d6806ebc570bc
opaque-maniac <mbithimark8@gmail.com>
2023-10-31 15:38:22 +0300

    finished the greedy algorithm

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this is the main function
+ * @argc: number of commandline arguements
+ * @argv: an array of string commandline arguements
+ *
+ * Return: 0 if successful, 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int ct, n = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	ct = atoi(argv[1]);
+
+	while (ct > 0)
+	{
+		n++;
+		if ((ct -25) >= 0)
+		{
+			ct -= 25;
+			continue;
+		}
+		if ((ct - 10) >= 0)
+		{
+			ct -= 10;
+			continue;
+		}
+		if ((ct - 5) >= 0)
+		{
+			ct -= 5;
+			continue;
+		}
+		if ((ct -2) >= 0)
+		{
+			ct -= 2;
+			continue;
+		}
+		ct--;
+	}
+	printf("%d\n", n);
+	return (0);
+}
