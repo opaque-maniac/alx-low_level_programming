55915d7d855948045804f0243bbfbbd823f4c3da
opaque-maniac <mbithimark8@gmail.com>
2023-11-07 11:18:29 +0300

    Finished the first question

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - this is a struct for do
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
